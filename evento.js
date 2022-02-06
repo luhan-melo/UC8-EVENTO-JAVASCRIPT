b1e8b547df3702bfa04f64af8ef1c020b702198e
Luhan Melo <luhanalves@outlook.com>
2022-02-06 13:27:20 -0300

    revisão do código conforme orientações do encontro remoto do dia 26.01.2022

--- evento.js
+++ evento.js
@@ -1,28 +1,25 @@
-let dataHoje = "03/01/2022";
-let dataEvento = "13/01/2021";
-console.log("A data de hoje é: " + dataHoje);
-console.log("A data do evento é: " + dataEvento);
-let verifDataEvento = "Sim"
-console.log("A data do evento é posterior a data atual? " + verifDataEvento )
-if (verifDataEvento == "Sim") {
-    let nomeEvento = "Webinar Logica de Programação"
-    let nomePalestrante1 = "Thiago"
-    let contador = 1
-        while (contador <= 100) {
-    let nomeParticipante = "Lucas"
-    let idadeParticipante = 18
-    console.log("Idade do participante é de: "+ idadeParticipante)
-    if (idadeParticipante >= 18) {
-        console.log("Participante poderá ser cadastrado")
+let dataHoje = new Date ()
+let dataEvento = new Date('03/10/2022')
+
+
+if (dataEvento > dataHoje) {
+    let nomePalestrante = "Thiago"
+    let idadeParticipantes = 18
+    if (idadeParticipantes >= 18) {
+
+        let listaParticipantes = ["Henrique", "Luisa", "Viviane"]
+        console.log("Os participantes do evento são:")
+        for (let contadorParticipantes = 0; contadorParticipantes <listaParticipantes.length; contadorParticipantes++){
+            console.log(listaParticipantes[contadorParticipantes])
+        }
     } else {
-        console.log("Idade do participante menor do que a mínima permitida")
-    }
-    contador = contador+1
+        console.log("Idade do participante menor do que a mínima permitida")}
+       
+let quantidadeParticipantes = 3
+if (quantidadeParticipantes > 100) {
+console.log("Não será mais permitido cadastrar participantes")
 }
-console.log("Limite de participantes atingido")
-console.log("O evento: ", nomeEvento)
-console.log("Será ministrado pelo(s) seguinte(s) palestrante(s): ", nomePalestrante1)
-console.log("Contando com os seguintes participantes:", nomeParticipante)
+console.log ("\nNome do palestrante:" + nomePalestrante)
 } else {
-    console.log("Data do evento inválida, evento não será permitido")
+    console.log("Data do evento inválida, evento não será permitido.")
 }
